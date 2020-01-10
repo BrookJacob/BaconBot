f94edb9ac5a1e12f38d1cf779399fde87da76a89
BrookJacob <brookjacob@gmail.com>
2020-01-10 16:03:50 -0700

    added conditional for msg.channel.type

--- bot.js
+++ bot.js
@@ -27,37 +27,41 @@ client.once('ready', () => {
 });
 
 client.on('message', msg => {
-    db.collection('guilds').doc(msg.guild.id).get().then((q) => {
-        if (q.exists) {
-            prefix = q.data().prefix;
-        }
-    }).then(() => {
-        if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-
-        const args = msg.content.slice(prefix.length).split(/ +/);
-        const commandName = args.shift();
-     
-        if (!client.commands.has(commandName)) return;
-
-        const command = client.commands.get(commandName);
-
-        if (command.args && !args.length) {
-            let reply = ` you didn't include any arguments.`;
-
-            if (command.usage) {
-                reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    if (msg.channel.type == 'text') {
+        db.collection('guilds').doc(msg.guild.id).get().then((q) => {
+            if (q.exists) {
+                prefix = q.data().prefix;
             }
+        });
+    }
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-            return msg.reply(reply);
-        }
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift();
     
-        try {
-            command.execute(client, msg, args, db, prefix);
-        } catch (error) {
-            console.error(error);
-            msg.reply(`There was an eror trying to execute that command: ${error}`);
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.guildOnly && msg.channel.type == 'dm') return msg.reply('That command can\'t be used in DMs.');
+
+    if (command.args && !args.length) {
+        let reply = ` you didn't include any arguments.`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
         }
-    });
+
+        return msg.reply(reply);
+    }
+
+    try {
+        command.execute(client, msg, args, db, prefix);
+    } catch (error) {
+        console.error(error);
+        msg.reply(`There was an eror trying to execute that command: ${error}`);
+    }
 });
 
 client.on('guildCreate', async gData => {
